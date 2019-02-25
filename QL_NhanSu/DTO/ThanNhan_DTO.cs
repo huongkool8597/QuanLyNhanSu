40af08b10b711e153c2a54afd547ae46fa8cb69c
Nguyen Huu Hieu <hieunh1801@gmail.com>
2019-02-26 00:57:48 +0700

    Thêm file quản lý nhân sự

---
+++ QL_NhanSu/DTO/ThanNhan_DTO.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QL_NhanSu.DTO
+{
+    class ThanNhan_DTO
+    {
+        private int maNV;
+        private string hoTenThanNhan;
+        private DateTime ngaySinh;
+        private string gioiTinh;
+        private string quanHe;
+
+        public int MaNV { get => maNV; set => maNV = value; }
+        public string HoTenThanNhan { get => hoTenThanNhan; set => hoTenThanNhan = value; }
+        public DateTime NgaySinh { get => ngaySinh; set => ngaySinh = value; }
+        public string GioiTinh { get => gioiTinh; set => gioiTinh = value; }
+        public string QuanHe { get => quanHe; set => quanHe = value; }
+
+        public ThanNhan_DTO(int maNV, string hoTenThanNhan, DateTime ngaySinh, string gioiTinh, string quanHe)
+        {
+            this.maNV = maNV;
+            this.hoTenThanNhan = hoTenThanNhan;
+            this.ngaySinh = ngaySinh;
+            this.gioiTinh = gioiTinh;
+            this.quanHe = quanHe;
+        }
+
+        public ThanNhan_DTO(DataRow row)
+        {
+            Int32.TryParse(row["MaNhanVien"].ToString(), out this.maNV);
+            this.hoTenThanNhan = row["HoTen"].ToString();
+            this.ngaySinh = (DateTime)row["NgaySinh"];
+            this.gioiTinh = row["GioiTinh"].ToString();
+            this.quanHe = row["QuanHe"].ToString();
+        }
+    }
+}
